4385976712145b978615bbc8824d1520eaa146eb
mcventur <marcelinopenide@gmail.com>
2024-01-02 10:37:10 +0100

    Ejemplo TCP Oracle

---
+++ EjemploTCP_Oracle/src/EchoClient.java
@@ -0,0 +1,48 @@
+/*
+Extraído de https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+Se deja de leer la entrada estándar y se terminan los envíos pulsando Ctrl + C si se ejecuta desde terminal
+*/
+
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            System.err.println(
+                    "Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        //Abre el socket y los streams de comunicación, y abre un flujo de lectura de teclado (System.in)
+        //para enviar lo leído por teclado al socket hasta pulsar Ctrl + C
+        try (
+                Socket echoSocket = new Socket(hostName, portNumber);
+                PrintWriter out =
+                        new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in =
+                        new BufferedReader(
+                                new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn =
+                        new BufferedReader(
+                                new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
+    }
+}
---
+++ EjemploTCP_Oracle/src/EchoServer.java
@@ -0,0 +1,40 @@
+/*
+Extraído de https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+ */
+
+import java.net.*;
+import java.io.*;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try (
+                ServerSocket serverSocket =
+                        new ServerSocket(Integer.parseInt(args[0]));
+                //La función accept de la instancia de ServerSocket devuelve una instancia de Socket
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out =
+                        new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+                //Añado esto para confirmar que los mensajes llegan
+                System.out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
