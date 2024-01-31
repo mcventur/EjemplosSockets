33acaf5b7a519bdcc1329f138fd900faf89bd0e5
mcventur <marcelinopenide@gmail.com>
2024-01-31 11:15:12 +0100

    Ajustes ejemplos InetAddress

--- Direcciones_InetAddress/src/TestInetAddress.java
+++ Direcciones_InetAddress/src/TestInetAddress.java
@@ -5,34 +5,39 @@ public class TestInetAddress {
     private static final String URL = "www.google.es";
 
     public static void main(String[] args) {
-        InetAddress dir = null;
+        InetAddress localhost = null;
+        InetAddress google = null;
         try {
             //Localhost
             System.out.println("==============================================");
             System.out.println("SALIDA PARA LOCALHOST");
-            dir = InetAddress.getLocalHost();
-            pruebaMetodos(dir);
+            localhost = InetAddress.getLocalHost();
+            //localhost = InetAddress.getByName("localhost");
+            pruebaMetodos(localhost);
 
             //www.google.es
             System.out.println("==============================================");
             System.out.println("SALIDA PARA UN NOMBRE DE HOST O URL");
-            dir = InetAddress.getByName(URL);
-            pruebaMetodos(dir);
-            //Array con todas las direcciones ip asignadas al mismo host
-            InetAddress[] direcciones = InetAddress.getAllByName(dir.getHostName());
-            for (InetAddress direccion : direcciones) {
-                System.out.println(direccion.toString());
-            }
+            google = InetAddress.getByName(URL);
+            pruebaMetodos(google);
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
     }
 
-    private static void pruebaMetodos(InetAddress dir) {
+    private static void pruebaMetodos(InetAddress dir) throws UnknownHostException {
         System.out.println("\tSalida toString(): " + dir);
         System.out.println("\tMétodo getHostName(): " + dir.getHostName());
         System.out.println("\tMétodo getHostAddress(): " + dir.getHostAddress());
         System.out.println("\tMétodo getCanonicalHostName(): " + dir.getCanonicalHostName());
+
+        //Array con todas las direcciones ip asignadas al mismo host
+        InetAddress[] direcciones = InetAddress.getAllByName(dir.getHostName());
+        System.out.println("Método getAllByName(): ");
+        for (InetAddress direccion : direcciones) {
+            System.out.println(direccion.toString());
+        }
+
     }
 }
